886f9e424c17737e4f3a269fded8dcd23e21d252
Nikhil Agarwal <nikhil12386@yahoo.com>
2021-02-20 10:43:52 +0530

    initial env commit with login auth

---
+++ proaccelerate/static/js/index.js
@@ -0,0 +1,58 @@
+// DOM elements
+const guideList = document.querySelector(".guides");
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+const accountDetails = document.querySelector(".account-details");
+
+const setupUI = (user) => {
+  if (user) {
+    db.collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        const html = `
+          <div>Logged in as ${user.email}</div>
+          <div>Name : ${doc.data().name}</div>
+          <div>Bio : ${doc.data().bio}</div>
+        `;
+        accountDetails.innerHTML = html;
+      });
+    // toggle user UI elements
+    loggedInLinks.forEach((item) => (item.style.display = "block"));
+    loggedOutLinks.forEach((item) => (item.style.display = "none"));
+  } else {
+    // clear account info
+    accountDetails.innerHTML = "";
+    // toggle user elements
+    loggedInLinks.forEach((item) => (item.style.display = "none"));
+    loggedOutLinks.forEach((item) => (item.style.display = "block"));
+  }
+};
+
+// setup guides
+const setupGuides = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const guide = doc.data();
+      const li = `
+            <li>
+              <div class="collapsible-header grey lighten-4"> ${guide.Title} </div>
+              <div class="collapsible-body white"> ${guide.Time} </div>
+            </li>
+          `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  } else {
+    guideList.innerHTML = '<h4 class="center-align">Login to view Todo</h4>';
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(items);
+});
